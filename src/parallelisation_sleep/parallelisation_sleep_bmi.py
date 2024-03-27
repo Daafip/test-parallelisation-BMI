9fc2c92c03a2c345289760b5110bb0c2412507fb
Daafip <daafips@gmail.com>
2024-03-27 12:24:15 +0100

    fix underestimation of "simple" model

--- src/parallelisation_sleep/parallelisation_sleep_bmi.py
+++ src/parallelisation_sleep/parallelisation_sleep_bmi.py
@@ -1,4 +1,5 @@
 from typing import Any, Tuple
+import time
 import numpy as np
 from parallelisation_sleep import utils
 from parallelisation_sleep.lumped_bmi import LumpedBmi
@@ -11,12 +12,23 @@ class ParallelisationSleep(LumpedBmi):
         # The model config contains the precipitation file, and the model parameter.
         self.config: dict[str, Any] = utils.read_config(config_file)
 
+        self.start_time = 0
+        self.current_timestep = self.start_time
+
+        self.end_timestep = 20_000_000
+
         # The one model parameter is the 'sleepiness' of the model:
         self.sleepiness = self.config["sleepiness"]
 
     def update(self) -> None:
 
-        time.sleep(self.sleepiness)
+        if self.current_timestep < self.end_timestep:
+            # Do calculations for current timestep
+
+            time.sleep(self.sleepiness)
+
+            # Advance the model time by one step
+            self.current_timestep += self.sleepiness
 
 
     def get_component_name(self) -> str:
@@ -46,3 +58,11 @@ class ParallelisationSleep(LumpedBmi):
 
     def get_output_var_names(self) -> Tuple[str]:
         return ("discharge",)
+
+    def get_start_time(self) -> float:
+        """Return end time in seconds since 1 january 1970."""
+        return get_unixtime(self.start_time) # type: ignore
+
+    def get_end_time(self) -> float:
+        """Return end time in seconds since 1 january 1970."""
+        return get_unixtime(self.end_timestep) # type: ignore
